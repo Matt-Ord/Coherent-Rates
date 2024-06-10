2704ee3dc1ed8e85359216335f896d063b8519de
Ziyoulu1 <zl494@cam.ac.uk>
2024-06-10 10:18:41 +0000

    add plot evolution

--- examples/main.py
+++ examples/main.py
@@ -1,7 +1,9 @@
-from coherent_rates.plot import plot_system_eigenstates
+from coherent_rates.plot import plot_system_eigenstates, plot_system_evolution
 from coherent_rates.system import HYDROGEN_NICKEL_SYSTEM, PeriodicSystemConfig
 
 if __name__ == "__main__":
-    config = PeriodicSystemConfig((1,), (100,), 2)
+    config = PeriodicSystemConfig((1,), (100,), 10)
 
     plot_system_eigenstates(HYDROGEN_NICKEL_SYSTEM, config)
+
+    plot_system_evolution(HYDROGEN_NICKEL_SYSTEM, config)
